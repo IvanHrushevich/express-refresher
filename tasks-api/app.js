aae331e1dce31cd470af608e3d1528c4aaae1701
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-08-18 02:49:29 +0300

    implement base API structure

--- tasks-api/app.js
+++ tasks-api/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App');
+const express = require('express');
+
+const tasks = require('./routes/tasks');
+
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
---
+++ tasks-api/controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req, res) => {
+  res.send('getAllTasks');
+};
+
+const createTask = (req, res) => {
+  res.send('createTask');
+};
+
+const getTask = (req, res) => {
+  res.send('getTask');
+};
+
+const updateTask = (req, res) => {
+  res.send('updateTask');
+};
+
+const deleteTask = (req, res) => {
+  res.send('deleteTask');
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
