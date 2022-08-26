634a045130b80fb062943288e548f04d4a55d6d7
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-08-26 17:11:18 +0300

    improve error handling

--- tasks-api/middleware/error-handler.js
+++ tasks-api/middleware/error-handler.js
@@ -1,5 +1,11 @@
-const errorHandler = (err, req, res) => {
-  return res.status(500).json({ message: err });
+const { CustomApiError } = require('../errors/custom-error');
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomApiError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: 'Something went wrong.' });
 };
 
 module.exports = errorHandler;
