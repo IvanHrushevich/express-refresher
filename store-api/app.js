bce95b8934fc7e51cbc080a59ff2a7e8c338fdf4
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-08-26 18:26:13 +0300

    implement initial express setup

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,35 @@
-console.log('test');
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1> <a href="/api/vi/products">Products route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
